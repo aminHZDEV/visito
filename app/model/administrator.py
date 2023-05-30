ea10cccb1ae0d9c4efcb2072e857912319bfd103
Mehdi Roudaki <mehdiroudaki@hotmail.com>
2023-05-30 04:48:09 +0330

    Add administrator model

---
+++ app/model/administrator.py
@@ -0,0 +1,53 @@
+__author__ = "Mehdi Roudaki"
+__copyright__ = "Copyright 2023"
+__credits__ = ["Kaveh Teymoury", "Hamid Moradi"]
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = ""
+__email__ = "mehdiroudaki@hotmail.com"
+__status__ = "Production"
+
+
+class Administrator:
+    def __init__(self, name: str = "", username: str = "", password: str = "", id_cart=None):
+        self._name = name
+        self._id_cart = id_cart
+        self._username = username
+        self._password = password
+
+    # Setters and Getters lie beyond this comment
+    # Name accessors
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, a):
+        self._name = a
+
+    # ID accessors
+    @property
+    def id_cart(self):
+        return self._id_cart
+
+    @id_cart.setter
+    def id_cart(self, a):
+        self._id_cart = a
+
+    # Username accessors
+    @property
+    def username(self):
+        return self._username
+
+    @username.setter
+    def username(self, a):
+        self._username = a
+
+    # Password accessors
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, a):
+        self._password = a
